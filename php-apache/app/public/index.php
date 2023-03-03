99ce4b4ad00d03dadd98bb765b0075ecbe7b4834
Kaori Tamashiro <k.tamashiro1212@gmail.com>
2023-03-03 14:33:37 +0900

    'Fix:虫食い部分修正'

--- php-apache/app/public/index.php
+++ php-apache/app/public/index.php
@@ -1,8 +1,7 @@
 <?php
 try {
-    $dbh = new PDO('mysql:dbname=mysql;host=localhost;unix_socket=/tmp/mysql.sock', 'root', 'EQpryctnz099');
+    $dbh = new PDO('mysql:dbname=mysql;host=review-mysql;unix_socket=/tmp/mysql.sock', 'root', 'e2QNqCUd');
     echo '<h1 style="color:yellow">Congratulations</h1>';
 } catch (PDOException $e) {
     echo '<h1 style="color:red">Failed<br><p>' . $e->getMessage() . '</p>';
 }
-
